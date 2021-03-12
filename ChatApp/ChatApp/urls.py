d3927f36402241b26d2d14574bbc5018a025325c
Bekzhan <bidahmetov2002@gmail.com>
2021-03-12 15:47:39 +0100

    Register New Users

--- ChatApp/ChatApp/urls.py
+++ ChatApp/ChatApp/urls.py
@@ -20,12 +20,17 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from personal.views import (
-	home_screen_view
+	home_screen_view,
+)
+
+from account.views import (
+	register_view,
 )
 
 urlpatterns = [
 	path('', home_screen_view, name='home'),
     path('admin/', admin.site.urls),
+    path('register/', register_view, name='register')
 ]
 
 if settings.DEBUG:
